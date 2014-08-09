73cc32ac53baf0c305bd17a3259fae740c5706fc
Mike McNeil <mike@balderdash.co>
2014-08-09 07:13:45 -0500

    sails generate api user login logout signup

---
+++ api/controllers/UserController.js
@@ -0,0 +1,39 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+
+  /**
+   * `UserController.login()`
+   */
+  login: function (req, res) {
+    return res.json({
+      todo: 'login() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.logout()`
+   */
+  logout: function (req, res) {
+    return res.json({
+      todo: 'logout() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.signup()`
+   */
+  signup: function (req, res) {
+    return res.json({
+      todo: 'signup() is not implemented yet!'
+    });
+  }
+};
+
